f8fa90c91ac24c222d2895642f68d3514bf251bd
KeerthanaJ1998 <63415476+KeerthanaJ1998@users.noreply.github.com>
2020-11-30 17:17:34 +0530

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,19 @@
+//Downloading libraries and unzipping file to load data
+
+library(ggplot2)
+library(RColorBrewer)
+# Download and unzip the file:
+dir.create("./air_pollution")
+urlzip <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+download.file(urlzip, destfile = "./air_pollution.zip" )
+unzip("./air_pollution.zip", exdir = "./air_pollution" )
+# Load the data:
+NEI <- readRDS("./air_pollution/summarySCC_PM25.rds")
+SCC <- readRDS("./air_pollution/Source_Classification_Code.rds")
+# Check NEI data
+str(NEI)
+
+//Plot1
+totalNEI <- aggregate(Emissions ~ year, NEI, sum)
+
+plot(totalNEI$year, totalNEI$Emissions, type = "o", col = "steelblue3", main = expression("Total US "~ PM[2.5]~ "Emissions by Year"), ylab = expression("Total US "~   PM[2.5] ~ "Emissions"), xlab = "Year")
